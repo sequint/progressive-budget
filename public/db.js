53a117e3d5e20333b9730e90e08d8d7be25f81df
Steven Quintana <steve.e.quint@gmail.com>
2021-10-07 15:44:47 -0700

    Create request function to check if an update is needed once back online

--- public/db.js
+++ public/db.js
@@ -1,6 +1,17 @@
 let db
 const request = indexedDB.open('budget', 1)
 
+request.onupgradeneeded = ({ target }) => {
+  db = target.result
+  db.createObjectStore('pending', { autoIncrement: true })
+}
 
+request.onsuccess = ({ target }) => {
+  db = target.result
+
+  if (navigator.onLine) {
+    checkDatabase()
+  }
+}
 
 window.addEventListener('online', checkDatabase)
