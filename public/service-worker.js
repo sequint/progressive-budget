f6e5b6e25e811e91868b58f772f10a2bdb1ad3d4
Steven Quintana <steve.e.quint@gmail.com>
2021-10-07 15:40:06 -0700

    Create service worker to handle data storage and get requests while offline

--- public/service-worker.js
+++ public/service-worker.js
@@ -4,3 +4,42 @@ const urls = [
   '/manifest.json',
   '/icons/icon-192x192.png'
 ]
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('transaction-cache-v1')
+      .then(cache => cache.addAll(urls)))
+})
+
+self.addEventListener('fetch', event => {
+
+  if (event.request.url.includes('/api/')) {
+    event.respondWith(
+      caches.open('transaction-data-cache-v1').then(cache => {
+        return fetch(event.request)
+          .then(res => {
+            if (res.status === 200) {
+              cache.put(event.request.url, res.clone())
+            }
+            return res
+          })
+          .catch(err => cache.match(event.request))
+      })
+    )
+    return
+  }
+
+  event.respondWith(
+    fetch(event.request)
+      .catch(err => {
+        console.log(err)
+        return caches.match(event.request).then(match => {
+          if (match) {
+            return match
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/')
+          }
+        })
+      })
+  )
+})
