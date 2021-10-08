2916736b19f87dda847247fd324b41d62f3f20e3
Steven Quintana <steve.e.quint@gmail.com>
2021-10-08 12:57:29 -0700

    Add url parser

--- db/index.js
+++ db/index.js
@@ -1,3 +1,5 @@
 const mongoose = require('mongoose')
 
-module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget') }
+module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
+  useNewUrlParser: true
+}) }
