1d0bfc80cac2026ead85e5cb639545ed00dc0975
Steven Quintana <steve.e.quint@gmail.com>
2021-10-08 12:44:38 -0700

    Delete redundant then statement preventing the server from being turned on

--- db/index.js
+++ db/index.js
@@ -3,6 +3,4 @@ const mongoose = require('mongoose')
 module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
   useNewUrlParser: true,
   useFindAndModify: false
-})
-  .then(connect => console.log('connected to mongo db'))
-  .catch(err => console.log('could not connect to mongodb')) }
+}) }
