4563cb07179275f97919254e2a22213abfba33b5
Steven Quintana <steve.e.quint@gmail.com>
2021-10-07 17:29:46 -0700

    Update budget to transaction vars

--- public/db.js
+++ public/db.js
@@ -3,6 +3,7 @@ const request = indexedDB.open('budget', 1)
 
 request.onupgradeneeded = ({ target }) => {
   db = target.result
+  console.log(db)
   db.createObjectStore('pending', { autoIncrement: true })
 }
 
@@ -15,22 +16,22 @@ request.onsuccess = ({ target }) => {
 }
 
 const saveRecord = record => {
-  const budget = db.budget(['pending'], 'readwrite')
-  const store = budget.objectStore('pending')
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
   store.add(record)
 }
 
 const checkDatabase = () => {
-  const budget = db.budget(['pending'], 'readwrite')
-  const store = budget.objectStore('pending')
+  const transaction = db.transaction(['pending'], 'readwrite')
+  const store = transaction.objectStore('pending')
   const getAll = store.getAll()
 
   getAll.onsuccess = () => {
     if (getAll.result.length > 0) {
       axios.post('/api/transaction/bulk', getAll.result)
         .then(() => {
-          const budget = db.budget(['pending'], 'readwrite')
-          const store = budget.objectStore('pending')
+          const transaction = db.transaction(['pending'], 'readwrite')
+          const store = transaction.objectStore('pending')
           store.clear()
         })
     }
