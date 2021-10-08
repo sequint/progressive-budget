520a30bc4d43b2a7fd5a7ef3a2fbb221b2e1856f
Steven Quintana <steve.e.quint@gmail.com>
2021-10-07 19:17:37 -0700

    Move service worker register function from html file to index js file

--- public/index.js
+++ public/index.js
@@ -1,3 +1,10 @@
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js')
+      .then(reg => console.log('Service Worker registered', reg))
+  })
+}
+
 let transactions = [];
 let myChart;
 
