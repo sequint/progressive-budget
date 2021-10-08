68e5fb2ff6580ef2acfedd3a1f6ab4890c0ca973
Steven Quintana <steve.e.quint@gmail.com>
2021-10-08 12:41:40 -0700

    Insert set usefind as false

--- db/index.js
+++ db/index.js
@@ -1,3 +1,8 @@
 const mongoose = require('mongoose')
 
-module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget') }
+module.exports = async function syncDB() { await mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/budget', {
+  useNewUrlParser: true,
+  useFindAndModify: false
+})
+  .then(connect => console.log('connected to mongo db'))
+  .catch(err => console.log('could not connect to mongodb')) }
